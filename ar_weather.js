3607dacf8bb0df40eeb0663e61f637435679a4e0
Xander50 <73202723+Xander50@users.noreply.github.com>
2024-05-05 19:51:56 -0400

    Add files via upload

---
+++ ar_weather.js
@@ -0,0 +1,26 @@
+let coordinates={}
+$(document).ready(function(){
+    get_coordinates();
+})
+
+function get_coordinates(){
+    let searchParams=new URLSearchParams(window.location.search)
+    if(searchParams.has('source') && searchParams.has('destination')){
+        let source=searchParams.get('source')
+        let destination=searchParams.get('destination')
+        coordinates.source_lat=source.split(";")[0]
+        coordinates.source_lon=source.split(";")[1]
+        coordinates.destination_lat=source.split(";")[0]
+        coordinates.destination_lon=source.split(";")[1]
+    }
+    else{
+        alert("coordinates not selected.")
+        window.history.back()
+    }
+}
+
+$(function () {
+    $("#navigate-button").click(function () {
+        window.location.href = `ar_weather.html?source=${latitude};${longitude}&destination=${destination[1]};${destination[0]}`
+    })
+})
